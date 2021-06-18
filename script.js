e84e2c9017afd0ca1a82b9942f89776294d72bfd
stefanelli990 <stefan.mohenski@gmail.com>
2021-06-18 17:51:19 +0200

    last change

--- script.js
+++ script.js
@@ -1,27 +1,2 @@
-const toggle = document.querySelector('.toggle');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const hamburgerToggle = document.querySelector('.hamburger-toggle');
-const hamburgerDropdownContent = document.querySelector('.hamburger-dropdown-content');
-const dropDownToggle = document.querySelector('.dropdown-toggle');
 
-// toggle hamburger menu
-toggle.addEventListener('click', () => {
-  toggle.classList.toggle('change');
-  if (hamburgerMenu.style.display === 'block') {
-    hamburgerMenu.style.display = 'none';
-  } else {
-    hamburgerMenu.style.display = 'block';
-  }
-});
-
-
-// toggle hamburger dropdown 
-hamburgerToggle.addEventListener('click', () => {
-  dropDownToggle.classList.toggle('active');
-  if(hamburgerDropdownContent.style.display === 'block') {
-      hamburgerDropdownContent.style.display = 'none';
-  } else {
-    hamburgerDropdownContent.style.display = 'block';
-  }
-});
 
